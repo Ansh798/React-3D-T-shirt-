9ea32e28750d5783ac16398245f35b79c374ee05
Ansh798 <amshumankumar798@gmail.com>
2023-07-28 15:30:56 +0530

    3d project

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://product-3d-ai.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
